e2e25f6f07f564efc8a49294f2156b6446d3995a
Erick Ricardo Hjort Colunga <ErickHjort@MacBuzz-Pro.local>
2022-09-29 19:24:00 -0500

    another commit

--- app.py
+++ app.py
@@ -1,1 +1,40 @@
-# Put your app in here.
+from crypt import methods
+from inspect import ArgSpec
+from flask import Flask, request
+app = Flask(__name__)# Put your app in here.
+
+@app.route('/add')
+def add():
+    """Adds a and b"""
+    a = request.args.get("a", type=int)
+    b = request.args.get("b", type=int)
+    output = a+b
+    output = str(output)
+    return output
+
+@app.route('/sub')
+def add():
+    """Adds a and b"""
+    a = request.args.get("a", type=int)
+    b = request.args.get("b", type=int)
+    output = a-b
+    output = str(output)
+    return output
+
+@app.route('/mult')
+def add():
+    """Adds a and b"""
+    a = request.args.get("a", type=int)
+    b = request.args.get("b", type=int)
+    output = a*b
+    output = str(output)
+    return output
+
+@app.route('/div')
+def add():
+    """Adds a and b"""
+    a = request.args.get("a", type=int)
+    b = request.args.get("b", type=int)
+    output = a/b
+    output = str(output)
+    return output
